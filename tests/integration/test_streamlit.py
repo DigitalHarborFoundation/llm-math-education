2590798ad41d307ad2cab697407320ea9cb1ae75
Zachary Levonian <zach@digitalharbor.org>
2023-08-14 12:46:16 -0700

    Integration test for basic hint generation flow

--- tests/integration/test_streamlit.py
+++ tests/integration/test_streamlit.py
@@ -1,8 +1,40 @@
 # Integration tests for the streamlit app,
 # including testing some of the associated utilities in `streamlit_app`
 
-from llm_math_education import retrieval_strategies
+from llm_math_education import prompt_utils, retrieval_strategies
+from llm_math_education.prompts import hints as hint_prompts
 from streamlit_app import auth_utils, data_utils
+
+
+def test_hint_generation():
+    hint_prompt_manager = prompt_utils.PromptManager()
+    slot_map = data_utils.create_hint_default_retrieval_slot_map()
+    retrieval_strategy = retrieval_strategies.MappedEmbeddingRetrievalStrategy(slot_map)
+    hint_prompt_manager.set_retrieval_strategy(retrieval_strategy)
+
+    hint_prompt_manager.get_retrieval_strategy().update_map(
+        {
+            "question": "Which true? A) True B) False",
+            "correct_answer": "A",
+            "incorrect_answer": "B",
+            "lesson": "Booleans are fun",
+        },
+    )
+    for hint_type in hint_prompts.intro_prompts.keys():
+        hint_prompt_manager.clear_stored_messages()
+        intro_messages = hint_prompts.intro_prompts[hint_type]["messages"]
+        hint_prompt_manager.set_intro_messages(intro_messages)
+        messages = hint_prompt_manager.build_query(None)
+        assert len(messages) >= 1
+
+        for expected_fill in ["rori_microlesson_texts", "openstax_subsection_texts", "question"]:
+            assert any(
+                expected_fill in slot_fill_dict and len(slot_fill_dict[expected_fill].strip()) > 0
+                for slot_fill_dict in hint_prompt_manager.recent_slot_fill_dict
+            ), f"{expected_fill} should have non-empty fill in {hint_prompt_manager.recent_slot_fill_dict}"
+
+        followup_messages = hint_prompt_manager.build_query("Follow-up question")
+        assert len(followup_messages) == len(messages) + 1
 
 
 def test_data_utils():
